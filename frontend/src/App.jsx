9d86fcf60eead14b33a571d6136a34a1f4ac1653
chhitijpradhan <chhitijpradhan13@gmail.com>
2025-02-08 10:46:27 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
+import Navbar from "./components/Navbar;
+
 
 function App() {
  
 
   return (
-    <>
-      
-        
-    </>
+    <div>
+
+      hii
+      <Navbar></Navbar>
+
+    </div>
   )
 }
 
